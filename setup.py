9d652e54fd99bbfc6b25b4b5ac98bb7449d54f50
Yuriy Romanyshyn <yuriy.romanyshyn.lv.ua@gmail.com>
2020-06-29 22:34:13 +0300

    renamed project

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 
 setuptools.setup(
-    name="mysql-connector-wrapper-pkg-yuriy-romanyshyn",
+    name="mysql-databases-pkg-yuriy-romanyshyn",
     version="0.0.1",
     author="Yuriy Romanyshyn",
     author_email="yuriy.romanyshyn.ua.lv@gmail.com",
-    description="mysql-connector driver wrapper",
+    description="mysql-databases connections manager",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     install_requires=["mysql-connector-python"],
